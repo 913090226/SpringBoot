37301dbd7ca193484382186ff96db7d702b1eae0
jerry <913090226@qq.com>
2016-12-20 18:14:23 +0800

    add

--- spring-boot-web/src/main/java/com/neo/web/EmployeeController.java
+++ spring-boot-web/src/main/java/com/neo/web/EmployeeController.java
@@ -3,13 +3,14 @@ package com.neo.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.neo.domain.Employee;
 import com.neo.domain.EmployeeRepository;
 
-@Controller
+
+@RestController
 public class EmployeeController {
 	@Autowired
 	private EmployeeRepository employeeRepository;
